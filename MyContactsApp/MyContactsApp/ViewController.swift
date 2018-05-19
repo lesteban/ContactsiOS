220f35abc056b7bd07b661fc4083fa0c7b1b6bad
Laura <laueste92@gmail.com>
2018-05-19 22:43:26 +0200

    Initial Commit
    Initial commit

---
+++ MyContactsApp/MyContactsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyContactsApp
+//
+//  Created by Laura Esteban  on 19/5/18.
+//  Copyright © 2018 lesteban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
